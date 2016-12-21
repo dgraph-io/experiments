f12b6d9b941cb302fe54028487859796753efcd0
Ashwin Ramesh <ashwin@dgraph.io>
2016-12-21 12:59:07 +1100

    Modify encoding

--- sortedencoding/sortedencoding.go
+++ sortedencoding/sortedencoding.go
@@ -8,15 +8,21 @@ import (
 )
 
 func main() {
-	arr := []int{1, 2, 3, 4, -1, -2, -3, 0, 234, 10000, 123, -1543}
+	a := 2<<24 + 10
+	b := -2<<24 - 1
+	arr := []int{a, b, 1, 2, 3, 4, -1, -2, -3, 0, 234, 10000, 123, -1543}
 	sarr := make([]string, 0)
 	for _, it := range arr {
 		buf := new(bytes.Buffer)
 		//Encode using bigendian.
+		if it < 0 {
+			buf.WriteByte(0)
+		} else {
+			buf.WriteByte(1)
+		}
 		err := binary.Write(buf, binary.BigEndian, int32(it))
 		b := buf.Bytes()
 		// Filp the last but so we can preserve ordering.
-		b[0] = ^b[0]
 		if err != nil {
 			fmt.Println(err)
 		}
@@ -28,9 +34,8 @@ func main() {
 	for _, it := range sarr {
 		var pi int32
 		fmt.Printf("%v ", []byte(it))
-		itOrig := []byte(it)
+		itOrig := []byte(it[1:])
 		// Flip it back before decoding
-		itOrig[0] = ^itOrig[0]
 		buf := bytes.NewReader(itOrig)
 		// Decode to get original value
 		err := binary.Read(buf, binary.BigEndian, &pi)
